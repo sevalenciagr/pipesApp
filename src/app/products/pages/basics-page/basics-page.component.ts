499cfb9e317fbc86a78e59074677884208bc672a
sevalenciagr <sevalenciagr@gmail.com>
2024-03-01 18:11:29 -0500

    Pipes

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string = 'sebastian';
+  public nameUpper: string = 'SEBASTIAN';
+  public fullName: string = 'SEbAsTiAN vaLENCiA';
+
 }
